fe7103e9dab35294107d5385e4bdb0ef44452c42
Едисеев Олег <ediseev.oleg@wb.ru>
2024-09-21 15:14:26 +0300

    add task 2: SandClock

--- hw_2024_09_19/PrintShapes/PrintShapes/Program.cs
+++ hw_2024_09_19/PrintShapes/PrintShapes/Program.cs
@@ -70,38 +70,46 @@ namespace PrintShapes
             height = height % 2 == 0 ? height + 1 : height;
 
             int midHeight = height / 2;
-            string[] rhomb = new string[height];
+            string[] sandClock = new string[height];
 
             for (int i = 0; i <= midHeight; i++)
             {
-                int spacesBefore = midHeight - i;
-                int spacesAfter = 2 * i - 1;
+                int spacesBefore = i;
+                int spacesInside = height - 2 * (i + 1);
+
+                if (spacesInside < -1) spacesInside = 1;
 
                 if (isFilled)
                 {
-                    if (i == 0)
+                    if (i == midHeight)
                     {
-                        rhomb[i] = new string(' ', spacesBefore) + symbol;
+                        sandClock[i] = new string(' ', spacesBefore) + new string(symbol, height - 2 * spacesBefore);
                     }
                     else
                     {
-                        rhomb[i] = new string(' ', spacesBefore) + symbol + new string(symbol, spacesAfter) + symbol;
+                        sandClock[i] = new string(' ', spacesBefore) + symbol + new string(symbol, spacesInside) + symbol;
                     }
                 }
                 else
                 {
-                    if (i == 0)
+                    if (i == 0 || i == height)
                     {
-                        rhomb[i] = new string(symbol, spacesBefore) + symbol + new string(symbol, spacesBefore);
+                        sandClock[i] = new string(' ', spacesBefore) + new string(symbol, height - 2 * spacesBefore);
+                    }
+                    else if (i == midHeight)
+                    {
+                        sandClock[i] = new string(' ', spacesBefore) + new string(symbol, height - 2 * spacesBefore);
                     }
                     else
-                        rhomb[i] = new string(symbol, spacesBefore) + symbol + new string(' ', spacesAfter) + new string(symbol, spacesBefore) + symbol;
+                    {
+                        sandClock[i] = new string(' ', spacesBefore) + symbol + new string(' ', spacesInside) + symbol;
+                    }
                 }
 
-                rhomb[height - i - 1] = rhomb[i];
+                sandClock[height - i - 1] = sandClock[i];
             }
 
-            return rhomb;
+            return sandClock;
         }
 
         static void PrintSandClock(string[] sandClock)
@@ -127,11 +135,22 @@ namespace PrintShapes
 
             PrintRhomb(rhomb);
 
-            /*int height = 7;
-            char symbol = '*';
-            bool isFilled = true;
+
+            Console.WriteLine();
+
+
+            Console.Write("Введите высоту песочных часов (нечетное число > 3): ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите символ, из которого будeт состоять песочные часы: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Залить фигуру? (y/n): ");
+            isFilled = Console.ReadLine() == "y" ? true : false;
+
             string[] sandClock = GetSandClock(height, symbol, isFilled);
-            PrintSandClock(sandClock);*/
+
+            PrintSandClock(sandClock);
         }
     }
 }
